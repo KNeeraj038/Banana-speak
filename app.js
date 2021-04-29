6d82df94fd03201ebfb5aa0c80990dc7dece06cc
Kumar Neeraj <krneeraj038@gmail.com>
2021-04-29 21:00:08 +0530

    API call is done
    feat : Added API call to it.
    Working now.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Testing");
+var btnTranslate = document.querySelector("#btnTranslate");
+
+
+function constructUrl(inputString){
+    // const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+    const serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+    return serverUrl + "?" + "text="+ inputString;
+}
+
+function errorHandler(error){
+    Console.log("Error occured ! : "+error);
+}
+function clickBtnHandler(){
+    var inputString = txtInput.value;
+    
+    // construct url 
+    var url = constructUrl(inputString);
+    fetch(url)
+    .then(response => response.json())
+    .then(json =>{
+        txtOutput.innerHTML = json.contents.translated
+    })
+    .catch(errorHandler)
+
+    // console.log(inputString);
+}
+
+btnTranslate.addEventListener("click", clickBtnHandler);
